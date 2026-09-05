900cce9c2d64d48205825998a753c221db7d3edc
Gabo <gruposalas2025@gmail.com>
2026-09-05 10:12:02 -0500

    Add .gitignore, remove build/ from tracking

--- build/gigaspark_m4/modules/libmetal/libmetal/lib/processor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabo/zephyr-ws/modules/hal/libmetal/libmetal/lib/processor
-# Build directory: /home/gabo/gigaspark_os/build/gigaspark_m4/modules/libmetal/libmetal/lib/processor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gigaspark_os/modules/libmetal/libmetal/lib/compiler/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabo/zephyr-ws/modules/hal/libmetal/libmetal/lib/compiler
-# Build directory: /home/gabo/gigaspark_os/build/gigaspark_os/modules/libmetal/libmetal/lib/compiler
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("armcc")
-subdirs("gcc")
-subdirs("iar")
